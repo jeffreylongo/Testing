8346114c2af116c653088cb9681d71eae9a93def
Jeff Longo <jeffreylongo@yahoo.com>
2017-05-10 20:29:06 -0400

    files

---
+++ Testing/PanagramMachine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    class PanagramMachine
+    {
+        public void Main(String[] args)
+        {
+            string s = Console.ReadLine();
+            bool newS = s.ToLower().Where(c => Char.IsLetter(c)).GroupBy(c => c).Count() == 26;
+            if (newS)
+            {
+                Console.WriteLine(("pangram"));
+            }
+            else
+            {
+                Console.WriteLine("not pangram");
+            }
+        }
+    }
+}
---
+++ UnitTestProject1/PanTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Testing;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class PanTests
+    {
+        [TestMethod]
+        public void PangramTest()
+        {
+            // var PanMachine = new PanagramMachine();
+            var words = "typing words and words and words";
+            var checker = PanagramMachine.Main(words);
+            Assert.AreEqual("not pangram", checker);
+        }
+        [TestMethod]
+        public void PangramTest1()
+        {
+            // var PanMachine = new PanagramMachine();
+            var words = "typing words and words and words";
+            var checker = PanagramMachine.Main(words);
+            Assert.AreEqual("not pangram", checker);
+        }
+        [TestMethod]
+        public void PangramTest2()
+        {
+            // var PanMachine = new PanagramMachine();
+            var words = "typing words and words and words";
+            var checker = PanagramMachine.Main(words);
+            Assert.AreEqual("not pangram", checker);
+        }
+        [TestMethod]
+        public void PangramTest3()
+        {
+            // var PanMachine = new PanagramMachine();
+            var words = "typing words and words and words";
+            var checker = PanagramMachine.Main(words);
+            Assert.AreEqual("not pangram", checker);
+        }
+        [TestMethod]
+        public void PangramTest4()
+        {
+            // var PanMachine = new PanagramMachine();
+            var words = "typing words and words and words";
+            var checker = PanagramMachine.Main(words);
+            Assert.AreEqual("not pangram", checker);
+        }
+    }
+}
